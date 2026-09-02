adea923d97705a3d02b59dda3f3ebd2536288b66
BrianTranGit <xxbrianplayz@gmail.com>
2026-09-02 16:49:42 +1000

    added some seed data

--- student-5/database/init_db.py
+++ student-5/database/init_db.py
@@ -58,11 +58,18 @@ def init_db():
 
     # Seed Data for tickets
     ticket_data = [
-        (1, 1, 1, "CPU", "The CPU pins were bent upon arrival and opening.")
-        (2, 2, 2, "CPU", "The CPU doesn't work.")
-        (3, 3, 3, "GPU", "The GPU blew up.")
-        (4, 4, 4, "PC Case", "The case came in chipped.")
-        (5, 5, 5, "Motherboard", "It doesn't work.")
-        (6, 6, 6, "GPU", "The GPU came in chipped, upon use, the GPU was overheating and eventually blew up most likely due to the chip and fans not spinning as fast.")
-        (7, 7, 7, "Motherboard", "The motherboard has faulty LED lighting and also visible rust on the metal CPU clipping.")
+        (1, 1, 1, "CPU", "The CPU pins were bent upon arrival and opening.", "Pending")
+        (2, 2, 2, "CPU", "The CPU doesn't work.", "Pending")
+        (3, 3, 3, "GPU", "The GPU blew up.", "Pending")
+        (4, 4, 4, "PC Case", "The case came in with the front corner of the case being chipped.", "Pending")
+        (5, 5, 5, "Motherboard", "It doesn't work.", "Pending")
+        (6, 6, 6, "GPU", "The GPU came in chipped, upon use, the GPU was overheating and eventually blew up most likely due to the chip and fans not spinning as fast.", "Pending")
+        (7, 7, 7, "Motherboard", "The motherboard has faulty LED lighting and also visible rust on the metal CPU clipping.", "Pending")
+        (8, 8, 8, "PC Case", "The case looks ugly.", "Pending")
+        (9, 9, 9, "Hard Drive", "Hard drive is faulty and is corrupted, it is unusable.", "Pending")
+        (10, 10, 10, "Hard Drive", "It is slow.", "Pending")
     ]
+    cursor.executemany("""
+        INSERT INTO tickets (ticket_id, customer_id, product_id, product_category, ticket_claim, ticket_status)
+        VALUES (?, ?, ?, ?, ?, ?)
+    """, ticket_data)
